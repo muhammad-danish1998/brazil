ad8910d4cbb1b5a5bfdd35cb2d4ac0b25badac96
muhammad danish <iamdanish1998@gmail.com>
2022-01-27 22:39:36 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import Home from './component/Home';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Price from './pages/Price'
+import Service from './pages/Service'
+import Error from './pages/Error'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/service' component={Service} />
+        <Route exact path='/price' component={Price} />
+        <Route exact path='/contact' component={Contact} />
+        <Route  component={Error} />
+    </Switch>
+    </>
   );
 }
 
